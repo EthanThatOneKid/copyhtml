096b860f62590bc49baa17ad5fd8355840e3aa24
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-01-13 11:49:13 -0800

    Bundle Showdown in the `copymd` bookmarklet
    Resolves:
    - <https://github.com/EthanThatOneKid/copyhtml/pull/1#discussion_r1069090541>
    - <https://github.com/EthanThatOneKid/copyhtml/pull/1#discussion_r1069090605>

--- examples/md/copymd.ts
+++ examples/md/copymd.ts
@@ -1,13 +1,16 @@
+/** @see https://skypack.dev/view/showdown */
+import { Converter } from "https://cdn.skypack.dev/-/showdown@v2.1.0-VOdouPaBMsLwxZmH1dtQ/dist=es2019,mode=imports/optimized/showdown.js";
+
 import { copyHTML } from "../../copyhtml/copyhtml.ts";
 
 export const copyMD = copyHTML.bind(null, {
   transform(elements: NodeListOf<Element>) {
-    const html = elements[0]?.outerHTML;
+    const html = elements[0]?.innerHTML;
     if (!html) {
       return;
     }
 
-    const converter = new window.showdown.Converter();
+    const converter = new Converter();
     const converted = converter.makeMarkdown(html);
     console.log("Custom MD transformation!", { html, converted });
     return converted;
--- examples/md/main.ts
+++ examples/md/main.ts
@@ -1,13 +1,4 @@
-import { dynamicRequireShowdown } from "https://etok.codes/dynamic_require/raw/main/examples/showdown/mod.ts";
-
 import { copyMD } from "./copymd.ts";
 
-dynamicRequireShowdown()
-  .then((prepared) => {
-    if (!prepared) {
-      throw new Error("Showdown not prepared!");
-    }
-
-    return copyMD();
-  })
+copyMD()
   .catch((err) => console.error(err));
